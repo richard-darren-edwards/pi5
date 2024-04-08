5f816acdd748af30d357a82f9e54b9de0527e77c
richard-darren-edwards <113705143+richard-darren-edwards@users.noreply.github.com>
2024-04-08 21:51:49 +0100

    Update setup_pi.sh

--- setup_pi.sh
+++ setup_pi.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 apt-get update
 apt-get upgrade
+sudo apt install libreoffice
+sudo apt install code
+wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
+sudo apt install gimp
+sudo apt install git
