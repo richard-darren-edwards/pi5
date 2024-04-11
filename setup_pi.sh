80f5aa218ae306a063074205e8cf85dea1516339
richard-darren-edwards <113705143+richard-darren-edwards@users.noreply.github.com>
2024-04-11 18:20:10 +0100

    Update setup_pi.sh

--- setup_pi.sh
+++ setup_pi.sh
@@ -6,3 +6,16 @@ sudo apt install code
 wget -qO- https://raw.githubusercontent.com/Botspot/pi-apps/master/install | bash
 sudo apt install gimp
 sudo apt install git
+mkdir /home/re/projects
+mkdir /home/re/projects/Tars
+#now set up git
+cd /home/re/projects
+
+pip install virtualenv
+cd /home/re/projects/HomeHub
+python3 -m venv venv
+
+curl -sSL https://get.docker.com | sh
+sudo usermod -aG docker $USER
+exit
+
